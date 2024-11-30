109bb4af8116a8cc376114aa7affd2b3e889fd19
+ <+>
2024-11-30 15:19:09 -0500

    Reorganized everything so we know whats placed where

---
+++ server/models/category.py
@@ -0,0 +1,22 @@
+from sqlalchemy_serializer import SerializerMixin, validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from config import db
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    @validates("name")
+    def validate_name(self, _, value):
+        if not value:
+            raise ValueError("name must be present")
+        if len(value) < 3:
+            raise ValueError("name must be at least 3 characters long")
+        return value
+
+    def __repr__(self):
+        return f'<Category {self.id}: {self.name}>'
+
+
