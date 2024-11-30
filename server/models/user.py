6fb92801d8a90ae2a9126577d35bedc502b5d9d2
+ <+>
2024-11-30 15:41:01 -0500

    Added a init file to the models folder and routes folder

--- server/models/user.py
+++ server/models/user.py
@@ -1,6 +1,4 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-from config import db
+from models.__init__ import SerializerMixin, validates, re, db
 
 
 class User(db.Model, SerializerMixin):
