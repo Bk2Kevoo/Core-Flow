74bb347bb7ac90f51f94993d3f8cd9776dc99ad6
+ <+>
2024-12-04 15:23:56 -0500

    Fixed the routes for existing exercise foler, get all and by its id

--- server/routes/exercise/exercise.py
+++ server/routes/exercise/exercise.py
@@ -1,10 +1,10 @@
-from routes.__init__ import Resource
+from routes.__init__ import Resource, make_response
 from models.exercise import Exercise
 
 class Exercises(Resource):
     def get(self):
         try:
             serialized_exercises = [exercise.to_dict() for exercise in Exercise.query]
-            return serialized_exercises, 200
+            return make_response(serialized_exercises, 200)
         except Exception as e:
-            return str(e), 400
+            return make_response({"error": str(e)}, 400)
